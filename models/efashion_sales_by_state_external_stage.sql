a43d6aa94181b00a644f77c0fc01f97668ec8a2b
KLC <kevin.lecorvaisier@nextdecision.fr>
2023-04-13 16:55:56 +0200

    type numeric forcé sur sales revenue

--- models/efashion_sales_by_state_external_stage.sql
+++ models/efashion_sales_by_state_external_stage.sql
@@ -3,7 +3,7 @@
 with gold_sales_external as (
     select 
     t.$4 as state,
-    sum(t.$9) as sales_revenue
+    sum(t.$9::numeric(15,2)) as sales_revenue
     from @ext_efashion (file_format => csv) t
     group by state
 )
